34ee39c8f4640ad8a522d761c8c6d9c7d86c6117
bradiepoa <bradiepoa7@gmail.com>
2023-08-12 18:23:40 +0300

    creeted and testing data

---
+++ src/components/ListItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ListItem = () => {
+    return (
+        <div>
+            <h1>List item one</1>
+            <h1>List item two</1>
+            <h1>List item Three</1>
+        </div>
+    );
+}
+
+export default ListItem;
+
--- src/components/assets/data.js
+++ src/components/assets/data.js
@@ -3,8 +3,20 @@ let blogs = [
         "id":1,
         "title":"testing",
         "body":"testing fist blog, i want to see if it will really display",
-        "date":"202-o6-09",
+        "date":"2022-06-09",
     },
+    {
+        "id":2,
+        "title":"testing2",
+        "body":"testing fist blog, i want to see if it will really display",
+        "date":"2023-06-09",
+    },
+    {
+        "id":3,
+        "title":"testing3",
+        "body":"testing fist blog, i want to see if it will really display",
+        "date":"2024-06-09",
+    }
 
 ]
 
